b101df02b67aa7ee98ef3c345490669eda07e0f6
Anika Christopher <anika.mchristopher@gmail.com>
2019-11-08 02:57:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,34 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  
+  def match(array)
+  if self.match %w(some_word.split(" ") == array.split(" ")) 
+    return "match"
+  else 
+    return []
+  end  
+  end
+  
+#     if word.match %w(some_word.split == ["anagrams"])
+#     return [matches]
+#   else #if not then
+#     return []
+#     end 
+#   end
+# end 
+
+# self 
+
+#   def match(anagrams)
+#   if 
+#   else
+#   return[]
+#   end 
+end
