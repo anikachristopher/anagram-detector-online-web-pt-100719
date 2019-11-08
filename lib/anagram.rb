809dddc702829e5bad6713c7de60335534e3af2c
Anika Christopher <anika.mchristopher@gmail.com>
2019-11-08 03:18:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,20 +1,20 @@
-# Your code goes here!
-class Anagram
+ # Your code goes here!
+# class Anagram
 
-attr_accessor :word 
+# attr_accessor :word 
   
-  def initialize(word)
-    @word = word
-  end
+#   def initialize(word)
+#     @word = word
+#   end
   
-  def match(array)
-    array.select do |word|
-      if word.split(" ").sort == @self.split(" ").sort
-      word
-      end
-    end
-  end
-end
+#   def match(array)
+#     array.select do |word|
+#       if word.split(" ").sort == @self.split(" ").sort
+#       word
+#       end
+#     end
+#   end
+# end
   
   
 class Anagram 
